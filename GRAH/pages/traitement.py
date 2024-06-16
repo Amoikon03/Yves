6b5e9301334c06f7adb004906ec8f4013cca3d94
Amoikon03 <164282850+Amoikon03@users.noreply.github.com>
2024-06-16 17:47:31 +0000

    Update traitement.py

--- GRAH/pages/traitement.py
+++ GRAH/pages/traitement.py
@@ -6,19 +6,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 
+# Affichez un message initial pour l'utilisateur
+st.write("Installez les packages nécessaires, importez vos données et effectuez la phase d'exploration de base des données")
 
-
-
-st.write("installez les packages necessaires et importez vos donnees et effectuez la phase d'exploration de base des donnees")
-
+# Charger les données depuis un fichier CSV
 df = pd.read_csv("Expresso_chun_dataset.csv")
 
+# Afficher les premières lignes du DataFrame
 st.write(df.head())
 
-st.write("Afficher des informations generales sur l'ensemble de donnees")
-
-st.write(df.info())
-st.write("l'information générale se trouve au niveau du terminal")
+# Afficher des informations générales sur l'ensemble de données
+st.write("Afficher des informations générales sur l'ensemble de données")
+st.write(df.info())  # Ceci affiche des informations au niveau du terminal
+st.write("L'information générale se trouve au niveau du terminal")
 
 # Créer des rapports de profilage de pandas pour obtenir des informations sur l'ensemble de données
 st.write("Rapport de profilage de l'ensemble de données :")
@@ -29,21 +29,19 @@ missing_values = df.isnull().sum()
 st.write("Valeurs manquantes dans les données :")
 st.write(missing_values)
 
+# Remplacer les valeurs manquantes par le mode ou la moyenne selon le type de données
 for col in df.columns:
     if df[col].dtype == 'object':
         # Si la colonne est catégorielle, remplacez les valeurs manquantes par le mode
         mode_val = df[col].mode()[0]
         df[col].fillna(mode_val, inplace=True)
-
     elif df[col].dtype in ['int64','float64']:
         # Sinon, si c'est numérique, remplacez les valeurs manquantes par 0
         mean_val = 0
         df[col].fillna(mean_val, inplace=True)
 
-    else:
-        pass
-st.write("Vérifions si elles ont bien été géré :")
-
+# Vérifier si les valeurs manquantes ont été bien gérées
+st.write("Vérifions si elles ont bien été gérées :")
 missing_values = df.isnull().sum()
 st.write(missing_values)
 
@@ -51,39 +49,34 @@ st.write(missing_values)
 df.drop_duplicates(inplace=True)
 st.write("Doublons supprimés. Nouvelle taille de l'ensemble de données :", df.shape)
 
+# Gérer les valeurs aberrantes, si elles existent
 st.write("Gérer les valeurs aberrantes, si elles existent")
-# Fonction pour détecter les valeurs aberrantes basées sur le z-score
 def detect_outliers_zscore(data, threshold=3):
-    outliers = []
-    z_scores = (df - df.mean()) / data.std()
-    outlier_indices = pd.abs(z_scores) > threshold
-    outliers = data[outlier_indices]
-    return outliers
+    z_scores = (data - data.mean()) / data.std()
+    outlier_indices = (z_scores.abs() > threshold).any(axis=1)
+    return data[outlier_indices]
+
+# Afficher les premières lignes du DataFrame après le traitement des valeurs aberrantes
 st.write(df.head(100))
 
-st.write("SELECTION DES COLONNES CATEGORIELLES")
+# Sélection des colonnes catégorielles
+st.write("Sélection des colonnes catégorielles")
+colonnes_categorielles = df.select_dtypes(include=['object']).columns
 
-colonnes_categoriell = df.select_dtypes(include=['object']).columns
-
-# Initialiser LabelEncoder
-st.write("ENCODAGE")
-
+# Encodage des colonnes catégorielles avec LabelEncoder
+st.write("Encodage des colonnes catégorielles")
 label_encoder = LabelEncoder()
-
-# Copie des données pour éviter les modifications sur les données originales
-#encoded_data = df.copy()
-
-label_encoder = LabelEncoder()
-for colonne in colonnes_categoriell:
+for colonne in colonnes_categorielles:
     df[colonne] = label_encoder.fit_transform(df[colonne])
 
-#df['CHURN'] = label_encoder.fit_transform(df["CHURN"])
 # Affichage des données encodées
-print("Données encodées :")
+st.write("Données encodées :")
 st.write(df.head(100))
 
-st.write("3.base sur l'entrainement d'exploration de donnees precedent et test d'un classificateur d'apprentissage automatique")
+# Préparation pour l'entraînement du modèle de classification
+st.write("Préparation pour l'entraînement et le test d'un classificateur d'apprentissage automatique")
 
+# Séparer les caractéristiques (features) et la cible (target)
 X = df.drop(['CHURN'], axis=1)
 y = df['CHURN']
 
@@ -99,13 +92,6 @@ classifier.fit(X_train, y_train)
 # Faire des prédictions sur l'ensemble de test
 y_pred = classifier.predict(X_test)
 
-
 # Calculer l'exactitude des prédictions
 accuracy = accuracy_score(y_test, y_pred)
-
-st.write("Accuracy Score:", accuracy)
-
-
-
-
-
+st.write("Score de précision (Accuracy Score) :", accuracy)
